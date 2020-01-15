3ae80dba998faf810bc6c8ff7033f842eb1588d2
Vicky Dineshchandra <vdineshc@cisco.com>
2020-01-15 22:01:55 +0000

    initial push

---
+++ static/js/random_photos.js
@@ -0,0 +1,12 @@
+// Randomly load a image in a given range
+function update_image(){
+    var image_num = Math.floor(Math.random() * 5) + 1;
+
+    var main_img = document.getElementById("main_photo");
+    main_img.src = "/static/images/cat_" + image_num + ".jpg";
+}
+
+const interval = setInterval(function() {
+    update_image()
+ }, 5000);
+
